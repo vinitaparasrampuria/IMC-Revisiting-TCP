7781f54351665d6f746f433c1a5793cb465068a1
Vinita Parasrampuria <91571551+vinitaparasrampuria@users.noreply.github.com>
2022-05-09 23:25:28 -0400

    Delete connect.py

--- connect.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-for i in range(10):
-	sock+str(i) = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-	sock+str(i).bind(('10.10.2.10', 4000+str(i))
-	sock+str(i).listen()
-	conn_sock+str(i), conn_addr+str(i) = sock+str(i).accept()
-
