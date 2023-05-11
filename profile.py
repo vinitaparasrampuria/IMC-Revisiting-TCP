2d09c9cd5d0d56a37f66f1f461395dd1ea80461a
Fraida Fund <ffund@nyu.edu>
2023-05-11 15:21:46 -0400

    update cloudlab profile to install iperf3 automatically on endpoints

--- profile.py
+++ profile.py
@@ -62,6 +62,7 @@ for i in range(params.n):
         node_sender.hardware_type = params.endtype
     node_sender.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD'
     node_sender.installRootKeys(True, True)
+    node_sender.addService(pg.Execute(shell="bash", command="sudo apt-get update; sudo apt-get -y install iperf3"))
     iface0 = node_sender.addInterface('interface-send-' + str(i), pg.IPv4Address('10.10.1.1' + str(i) ,'255.255.255.0'))
     iface0.bandwidth = 10000000
     link_0.addInterface(iface0)
@@ -75,6 +76,7 @@ for i in range(params.n):
         node_receiver.hardware_type = params.endtype
     node_receiver.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD'
     node_receiver.installRootKeys(True, True)
+    node_receiver.addService(pg.Execute(shell="bash", command="sudo apt-get update; sudo apt-get -y install iperf3"))
     iface0 = node_receiver.addInterface('interface-recv-' + str(i), pg.IPv4Address('10.10.2.1' + str(i),'255.255.255.0'))
     iface0.bandwidth = 10000000
     link_1.addInterface(iface0)
