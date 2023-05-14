561757bb09352c33f470898a579cef26cdee48dc
Vinita Parasrampuria <91571551+vinitaparasrampuria@users.noreply.github.com>
2023-05-13 22:13:52 -0400

    Create set-delay.sh

---
+++ endpoint-scripts/set-delay.sh
@@ -0,0 +1,7 @@
+#delay to be added
+delay=$1
+shift
+
+receiver_inf_name=$( ip route get 10.10.2.100 | grep -oP "(?<=dev )[^ ]+" );
+sudo tc qdisc del dev $receiver_inf_name root netem
+sudo tc qdisc add dev $receiver_inf_name root netem delay $delay
