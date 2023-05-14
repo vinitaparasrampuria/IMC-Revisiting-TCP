569abfaec116b8483bb6c75deca41ddc86b8d6cb
Vinita Parasrampuria <91571551+vinitaparasrampuria@users.noreply.github.com>
2023-05-13 23:57:12 -0400

    Update time unit

--- endpoint-scripts/set-delay.sh
+++ endpoint-scripts/set-delay.sh
@@ -4,4 +4,4 @@ shift
 
 receiver_inf_name=$( ip route get 10.10.2.100 | grep -oP "(?<=dev )[^ ]+" );
 sudo tc qdisc del dev $receiver_inf_name root netem
-sudo tc qdisc add dev $receiver_inf_name root netem delay $delay
+sudo tc qdisc add dev $receiver_inf_name root netem delay ${delay}ms
