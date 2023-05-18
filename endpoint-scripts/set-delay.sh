f78f68749362fac84e4097f517ea50efa6baf871
Vinita Parasrampuria <91571551+vinitaparasrampuria@users.noreply.github.com>
2023-05-17 23:35:13 -0400

    added command to show q status

--- endpoint-scripts/set-delay.sh
+++ endpoint-scripts/set-delay.sh
@@ -8,3 +8,5 @@ shift
 receiver_inf_name=$( ip route get 10.10.2.100 | grep -oP "(?<=dev )[^ ]+" );
 sudo tc qdisc del dev $receiver_inf_name root netem
 sudo tc qdisc add dev $receiver_inf_name root netem delay ${delay}ms limit ${limit}
+
+tc -s -d qdisc show dev $receiver_inf_name
