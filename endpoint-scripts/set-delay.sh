02bec5e487183ed5352c7c6f3f6f395d44ae7e06
Vinita Parasrampuria <91571551+vinitaparasrampuria@users.noreply.github.com>
2023-05-17 20:51:47 -0400

    added limit parameter

--- endpoint-scripts/set-delay.sh
+++ endpoint-scripts/set-delay.sh
@@ -2,6 +2,9 @@
 delay=$1
 shift
 
+limit=$1
+shift
+
 receiver_inf_name=$( ip route get 10.10.2.100 | grep -oP "(?<=dev )[^ ]+" );
 sudo tc qdisc del dev $receiver_inf_name root netem
-sudo tc qdisc add dev $receiver_inf_name root netem delay ${delay}ms
+sudo tc qdisc add dev $receiver_inf_name root netem delay ${delay}ms limit ${limit}
