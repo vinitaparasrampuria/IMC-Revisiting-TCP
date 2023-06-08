9c8414d85ab667a1030025950d73deb0ae23d85f
Vinita Parasrampuria <91571551+vinitaparasrampuria@users.noreply.github.com>
2023-06-08 14:44:13 -0400

    changed local_port value

--- endpoint-scripts/cwn_half.sh
+++ endpoint-scripts/cwn_half.sh
@@ -15,10 +15,11 @@ if [ ! -z "$sstxt" ]; then
 	do  
         #local_port=$(echo $line_first | awk '{print $5}{print $8}' | cut -f2 -d':'|tr -d '\n')
         
-        local_port=$(echo $line_first | awk '{print $5}' | cut -f2 -d':')
-        ino=$(echo $line_first | grep -oP '\bino:.*(\s|$)\bsk' | awk '{print $1}' | cut -f2 -d':')
+        #local_port=$(echo $line_first | awk '{print $5}' | cut -f2 -d':')
+        #ino=$(echo $line_first | grep -oP '\bino:.*(\s|$)\bsk' | awk '{print $1}' | cut -f2 -d':')
+	local_port=$(echo $line_first | awk '{print $5}{print $6}' | cut -f2 -d':' | tr -d '\n');
         current_cwnd=$(echo $line_second | grep -oP '\bcwnd:.*(\s|$)\bbytes_acked' | awk -F '[: ]' '{print $2}')
-		echo "$local_port$ino,$current_cwnd" >> sender-cwn-$ip-file.txt
+		echo "$local_port,$current_cwnd" >> sender-cwn-$ip-file.txt
 		done <<< "$sstxt"
 else
 	break
