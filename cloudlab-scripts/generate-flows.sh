bd4612cc5ff3b3c1e9361ef044e8bacde881fd5e
Vinita Parasrampuria <91571551+vinitaparasrampuria@users.noreply.github.com>
2023-06-08 16:14:39 -0400

    changed test_duration to [0-9] in grep

--- cloudlab-scripts/generate-flows.sh
+++ cloudlab-scripts/generate-flows.sh
@@ -167,24 +167,24 @@ else
 fi
 
 if [ $type == 1 ]; then
-   jfi=$(grep -r -E "[0-9].*0.00-${test_duration}.*sender" --include *${cca1}.txt /local/repository/cloudlab-scripts/result-${cca1} |tr '[' ' ' |awk -F ' ' '{sum+=$7}{sq+=$7*$7}{count+=1} END {print (sum*sum)/(sq*count)}')
-   sum=$(grep -r -E "[0-9].*0.00-${test_duration}.*sender" --include *${cca1}.txt /local/repository/cloudlab-scripts/result-${cca1} |tr '[' ' ' |awk -F ' ' '{sum+=$7}END {print sum}')
+   jfi=$(grep -r -E "[0-9].*0.00-[0-9].*sender" --include *${cca1}.txt /local/repository/cloudlab-scripts/result-${cca1} |tr '[' ' ' |awk -F ' ' '{sum+=$7}{sq+=$7*$7}{count+=1} END {print (sum*sum)/(sq*count)}')
+   sum=$(grep -r -E "[0-9].*0.00-[0-9].*sender" --include *${cca1}.txt /local/repository/cloudlab-scripts/result-${cca1} |tr '[' ' ' |awk -F ' ' '{sum+=$7}END {print sum}')
    echo sum of bandwidth is $sum Kbits/sec
-   square=$(grep -r -E "[0-9].*0.00-${test_duration}.*sender" --include *${cca1}.txt /local/repository/cloudlab-scripts/result-${cca1} |tr '[' ' ' |awk -F ' ' '{sum+=$7*$7}END {print sum}')
+   square=$(grep -r -E "[0-9].*0.00-[0-9].*sender" --include *${cca1}.txt /local/repository/cloudlab-scripts/result-${cca1} |tr '[' ' ' |awk -F ' ' '{sum+=$7*$7}END {print sum}')
    echo square is $square
-   count=$(grep -r -E "[0-9].*0.00-$test_duration.*sender" --include *${cca1}.txt /local/repository/cloudlab-scripts/result-${cca1} |tr '[' ' ' |awk -F ' ' '{count+=1}END {print count}')
+   count=$(grep -r -E "[0-9].*0.00-[0-9].*sender" --include *${cca1}.txt /local/repository/cloudlab-scripts/result-${cca1} |tr '[' ' ' |awk -F ' ' '{count+=1}END {print count}')
    echo count of $cca1 flows is $count
    echo JFI is $jfi
 
 elif [ $type == 2 ] || [ $type == 3 ];
 then
-   sum1=$(grep -r -E "[0-9].*0.00-${test_duration}.*sender" --include *${cca1}.txt /local/repository/cloudlab-scripts/result-${cca1}-${cca2} |tr '[' ' ' |awk -F ' ' '{sum+=$7} END {print sum}')
-   count1=$(grep -r -E "[0-9].*0.00-$test_duration.*sender" --include *${cca1}.txt /local/repository/cloudlab-scripts/result-${cca1}-${cca2} |tr '[' ' ' |awk -F ' ' '{count+=1}END {print count}')
+   sum1=$(grep -r -E "[0-9].*0.00-[0-9].*sender" --include *${cca1}.txt /local/repository/cloudlab-scripts/result-${cca1}-${cca2} |tr '[' ' ' |awk -F ' ' '{sum+=$7} END {print sum}')
+   count1=$(grep -r -E "[0-9].*0.00-[0-9].*sender" --include *${cca1}.txt /local/repository/cloudlab-scripts/result-${cca1}-${cca2} |tr '[' ' ' |awk -F ' ' '{count+=1}END {print count}')
    echo count of flows of $cca1 is $count1
    echo sum of Bandwidth of $cca1 is $sum1 Kbits/sec
 
-   sum2=$(grep -r -E "[0-9].*0.00-${test_duration}.*sender" --include *${cca2}.txt /local/repository/cloudlab-scripts/result-${cca1}-${cca2} |tr '[' ' ' |awk -F ' ' '{sum+=$7} END {print sum}')
-   count2=$(grep -r -E "[0-9].*0.00-$test_duration.*sender" --include *${cca2}.txt /local/repository/cloudlab-scripts/result-${cca1}-${cca2} |tr '[' ' ' |awk -F ' ' '{count+=1}END {print count}')
+   sum2=$(grep -r -E "[0-9].*0.00-[0-9].*sender" --include *${cca2}.txt /local/repository/cloudlab-scripts/result-${cca1}-${cca2} |tr '[' ' ' |awk -F ' ' '{sum+=$7} END {print sum}')
+   count2=$(grep -r -E "[0-9].*0.00-[0-9].*sender" --include *${cca2}.txt /local/repository/cloudlab-scripts/result-${cca1}-${cca2} |tr '[' ' ' |awk -F ' ' '{count+=1}END {print count}')
    echo count of flows of $cca2 is $count2
    echo sum of Bandwidth of $cca2 is $sum2 Kbits/sec
 else
