640588ef7ba98896afab273e9f6f69193b3dff63
Vinita Parasrampuria <91571551+vinitaparasrampuria@users.noreply.github.com>
2023-06-13 19:06:53 -0400

    added command to get rtt, dataseg and retrans

--- endpoint-scripts/cwn_half.sh
+++ endpoint-scripts/cwn_half.sh
@@ -18,8 +18,11 @@ if [ ! -z "$sstxt" ]; then
         #local_port=$(echo $line_first | awk '{print $5}' | cut -f2 -d':')
         #ino=$(echo $line_first | grep -oP '\bino:.*(\s|$)\bsk' | awk '{print $1}' | cut -f2 -d':')
 	local_port=$(echo $line_first | awk '{print $5}{print $6}' | cut -f2 -d':' | tr -d '\n');
+	rtt=$(echo $line_second | grep -oP '\brtt:.*(\s|$)\bmss' | awk '{print $1}' | cut -f2 -d':');
+	data_seg_out=$(echo $line_second | grep -oP '\bdata_segs_out:.*(\s|$)\bsend' | awk '{print $1}' | cut -f2 -d':');
+	retrans=$(echo $line_second | grep -oP '\bretrans:.*(\s|$)\brcv_space' | awk '{print $1}' | cut -f2 -d':');
         current_cwnd=$(echo $line_second | grep -oP '\bcwnd:.*(\s|$)\bbytes_acked' | awk -F '[: ]' '{print $2}')
-		echo "$local_port,$current_cwnd" >> sender-cwn-$ip-file.txt
+		echo "$local_port,$current_cwnd,$rtt,$data_seg_out,$retrans" >> sender-cwn-$ip-file.txt
 		done <<< "$sstxt"
 else
 	break
