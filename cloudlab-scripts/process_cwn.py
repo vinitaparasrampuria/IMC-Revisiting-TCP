ba694171ee30ff4571dde87ddf06e1b413ec22f6
Vinita Parasrampuria <91571551+vinitaparasrampuria@users.noreply.github.com>
2023-06-14 19:53:18 -0400

    removed unnecessary variables

--- cloudlab-scripts/process_cwn.py
+++ cloudlab-scripts/process_cwn.py
@@ -4,27 +4,24 @@ import numpy as np
 import csv
 from sklearn import metrics
 from sklearn.linear_model import LinearRegression
-import seaborn as sns
-sns.set()
-port_cwnd_half=[]
-ss_count=[]
+
+for i in range(1, len(sys.argv)):
+    print('argument:', i, 'value:', sys.argv[i])	
+senders=int(sys.argv[1])
+cca1=sys.argv[2]
+
 ports=[]
-packet_loss=[]
+
 y_values=[]
 x_values=[]
 x2_values=[]
 x3_values=[]
 x4_values=[]
-csv_filename='/local/repository/cloudlab-scripts/result-reno/packet_loss.csv'
-for i in range (0,10):
-  dat = pd.read_csv("/local/repository/cloudlab-scripts/result-reno/sender-cwn-10.10.2.1"+str(i)+"-file.txt", header=None, names=['port', 'cwnd','rtt','data_seg','retrans'])
-  dat_iperf= pd.read_csv("/local/repository/cloudlab-scripts/result-reno/output-10.10.2.1"+str(i)+".csv", header=None, names=['id','port','duration','time_unit','transfer_data','data_unit','bandwidth','BW_unit','retrans'] )
+csv_filename="/local/repository/cloudlab-scripts/result-"+cca1+"/packet_loss.csv"
+for i in range (0,senders):
+  dat = pd.read_csv("/local/repository/cloudlab-scripts/result-"+cca1+"/sender-cwn-10.10.2.1"+str(i)+"-file.txt", header=None, names=['port', 'cwnd','rtt','data_seg','retrans'])
+  dat_iperf= pd.read_csv("/local/repository/cloudlab-scripts/result-"+cca1+"/output-10.10.2.1"+str(i)+".csv", header=None, names=['id','port','duration','time_unit','transfer_data','data_unit','bandwidth','BW_unit','retrans'] )
   port_un=pd.unique(dat.port)
-  count=len(dat)
-  count_port=len(port_un)
-  ports.append(count_port)
-  ss_count.append(count)
-
 
   for p in port_un:
     dat_flow = dat[dat.port==p]
@@ -32,7 +29,6 @@ for i in range (0,10):
     x = dat_flow.cwnd.diff().values
     x[np.where(x==0)] = 1
     cwn_port_half=np.sum((np.diff(np.sign(x))) == -2)
-    port_cwnd_half.append(np.sum((np.diff(np.sign(x))) == -2))
     
     #method-1
     data_seg_out=dat_flow['data_seg'].iloc[len(dat_flow) - 1]
@@ -60,7 +56,6 @@ for i in range (0,10):
     
     packet_drop=total_retrans/data_seg_out
     #print(packet_drop)
-    packet_loss.append(packet_drop)
 
     x1=(1448*8*1000)/(avg_rtt*np.sqrt(packet_drop))
 
@@ -96,14 +91,6 @@ for i in range (0,10):
       x2_values.append(x2)
       x3_values.append(x3)
       x4_values.append(x4)
-
-
-    with open(csv_filename, 'a', newline='') as csvfile:
-      writer = csv.writer(csvfile)
-      columns = p, data_seg_out, meas_rtt, avg_rtt, meas_retrans, total_retrans, packet_drop, cwn_port_half, bandwidth_port, x1
-      writer.writerow(columns)
-
-
 
 x_values = np.array(x_values).reshape(-1, 1)
 y_values = np.array(y_values)
