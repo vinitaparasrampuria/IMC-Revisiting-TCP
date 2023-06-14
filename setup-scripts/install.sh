80ae494efb528f463c48f8aa4e7b689b9f505326
Vinita Parasrampuria <91571551+vinitaparasrampuria@users.noreply.github.com>
2023-06-14 17:23:58 -0400

    Create install.sh

---
+++ setup-scripts/install.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install -y python3-pip ethtool netcat moreutils
+pip3 install numpy
+pip3 install pandas
+pip3 install matplotlib
+pip3 install seaborn
+pip3 install sklearn
