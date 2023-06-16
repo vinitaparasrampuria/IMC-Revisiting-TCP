c091706f9bcb5e5105406958428fcdad85a2ef51
Vinita Parasrampuria <91571551+vinitaparasrampuria@users.noreply.github.com>
2023-06-16 14:51:36 -0400

    added code to check if file exists

--- cloudlab-scripts/process_cwn.py
+++ cloudlab-scripts/process_cwn.py
@@ -9,7 +9,7 @@ import seaborn as sns
 import matplotlib.backends.backend_pdf
 from matplotlib.backends.backend_pdf import PdfPages
 import sys
-
+import os
 
 for i in range(1, len(sys.argv)):
     print('argument:', i, 'value:', sys.argv[i])	
@@ -30,12 +30,23 @@ list_retrans1=[]
 list_retrans2=[]
 list_ratio1=[]
 list_ratio2=[]
-csv_filename="/local/repository/cloudlab-scripts/packet_loss.csv"
 
-#with open(csv_filename, 'a', newline='') as csvfile:
-#    writer = csv.writer(csvfile)
-#    header = 'port', 'data_seg_out', 'mean_rtt', 'bandwidth_port', 'port_retrans1', 'port_retrans2', 'cwn_half_port', 'packet_loss1', 'x1', 'packet_loss2', 'x2', 'cwnd_half_rate1','x3', 'cwnd_half_rate2', 'x4', 'ratio1', 'ratio2'
-#    writer.writerow(header)
+
+csv_filename="/local/repository/cloudlab-scripts/packet_loss.csv"
+if not os.path.isfile(csv_filename):
+    with open(csv_filename, 'a', newline='') as csvfile:
+      writer = csv.writer(csvfile)
+      header = 'port', 'data_seg_out', 'mean_rtt', 'bandwidth_port', 'port_retrans1', 'port_retrans2', 'cwn_half_port', 'packet_loss1', 'x1', 'packet_loss2', 'x2', 'cwnd_half_rate1','x3', 'cwnd_half_rate2', 'x4', 'ratio1', 'ratio2'
+      writer.writerow(header)
+   
+output_filename='/local/repository/cloudlab-scripts/output_mathis_C.csv'
+if not os.path.isfile(output_filename):
+    with open(output_filename, 'a', newline='') as csvfile:
+      writer = csv.writer(csvfile)
+      header = 'total_ports', 'ports', 'sum(y_values)', 'total_cwnd_half', 'total_retransmission1', 'total_retransmission2', 'total_retransmission1/total_cwnd_half', 'total_retransmission2/total_cwnd_half', 'np.nanmean(list_ratio1)', 'np.nanmean(list_ratio2)', 'reg_simple1.intercept_', 'reg_simple1.coef_[0]', 'reg_simple2.intercept_', 'reg_simple2.coef_[0]', 'reg_simple3.intercept_', 'reg_simple3.coef_[0]', 'reg_simple4.intercept_', 'reg_simple4.coef_[0]', 'router_dropped', 'router_sent', 'router_dropped/total_cwnd_half'
+      writer.writerow(header)
+
+
 for i in range (0,10):
   dat_cwn = pd.read_csv("sender-cwn-10.10.2.1"+str(i)+"-file.txt", header=None, names=['port', 'cwnd','rtt','data_seg','retrans'])
   dat_iperf= pd.read_csv("output-10.10.2.1"+str(i)+".csv", header=None, names=['id','port','duration','time_unit','transfer_data','data_unit','bandwidth','BW_unit','retrans'] )
@@ -145,11 +156,6 @@ reg_simple4 = LinearRegression(fit_intercept = False).fit(x4_values, y_values)
 print("Intercept: " , reg_simple4.intercept_)
 print("Coefficient list: ", reg_simple4.coef_)
 
-output_filename="/local/repository/cloudlab-scripts/output_mathis_C.csv"
-#with open(output_filename, 'a', newline='') as csvfile:
-#  writer = csv.writer(csvfile)
-#  header = 'total_ports', 'ports', 'sum(y_values)', 'total_cwnd_half', 'total_retransmission1', 'total_retransmission2', 'total_retransmission1/total_cwnd_half', 'total_retransmission2/total_cwnd_half', 'np.nanmean(list_ratio1)', 'np.nanmean(list_ratio2)', 'reg_simple1.intercept_', 'reg_simple1.coef_[0]', 'reg_simple2.intercept_', 'reg_simple2.coef_[0]', 'reg_simple3.intercept_', 'reg_simple3.coef_[0]', 'reg_simple4.intercept_', 'reg_simple4.coef_[0]', 'router_dropped', 'router_sent', 'router_dropped/total_cwnd_half'
-#  writer.writerow(header)
 
 with open(output_filename, 'a', newline='') as csvfile:
   writer = csv.writer(csvfile)
@@ -181,8 +187,8 @@ with PdfPages("/local/repository/cloudlab-scripts/linear_reg_plot.pdf") as pdf:
   plt.show()
   plt.close()
 
-  plt.scatter(x=x2_values[0:500].squeeze(), y=y_values[0:500], color='C4', s=10, label='actual values')
-  plt.scatter(x=x2_values[0:500].squeeze(), y=y_hat2[0:500], color='C3', s=10, label='predicted_values')
+  plt.scatter(x=x2_values.squeeze(), y=y_values, color='C4', s=10, label='actual values')
+  plt.scatter(x=x2_values.squeeze(), y=y_hat2, color='C3', s=10, label='predicted_values')
   plt.plot(x2_values.squeeze(), y_hat2, color='C2', linewidth=0.5, label='fit')
   plt.xlabel("x=mss/rtt*sqrt(packet_loss_rate)") 
   plt.ylabel("y=bandwidth(bits/sec)")
