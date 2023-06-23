013304f36c2e9b5b106f1126bf741e3b783afd04
Vinita Parasrampuria <91571551+vinitaparasrampuria@users.noreply.github.com>
2023-06-23 02:27:04 -0400

    install python

--- cloudlab-scripts/validate.sh
+++ cloudlab-scripts/validate.sh
@@ -13,9 +13,16 @@ done
 for i in {0..9}
 do
    sudo ssh -o StrictHostKeyChecking=no root@sender-$i /bin/bash << EOF
-   bash /local/repository/endpoint-scripts/change-queue.sh
-   bash /local/repository/endpoint-scripts/install-iperf.sh
-	nohup iperf3 -t 60 -P 10 -c 10.10.2.1$i > /dev/null 2>&1 &
+   bash /local/repository/endpoint-scripts/change-queue.sh 
+   bash /local/repository/endpoint-scripts/install-iperf.sh > /dev/null 2>&1 &
+   bash /local/repository/setup-scripts/install.sh > /dev/null 2>&1 &
+EOF
+done
+
+for i in {0..9}
+do
+   sudo ssh -o StrictHostKeyChecking=no root@sender-$i /bin/bash << EOF
+   nohup iperf3 -t 60 -P 10 -c 10.10.2.1$i > /dev/null 2>&1 &
 EOF
 done
 
