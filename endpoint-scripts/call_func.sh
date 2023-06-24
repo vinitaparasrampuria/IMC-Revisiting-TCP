01e3fed89544670c6f0f1b3ea9f4cf0b19cdeef8
Vinita Parasrampuria <91571551+vinitaparasrampuria@users.noreply.github.com>
2023-06-24 15:28:54 -0400

    commented call of cwn_hlf.sh

--- endpoint-scripts/call_func.sh
+++ endpoint-scripts/call_func.sh
@@ -5,7 +5,7 @@ while [ 1 ]
 do
 	if [ ! -z "$(ss --no-header -ein dst $ip)" ];
 	then
-	bash /local/repository/endpoint-scripts/cwn_half.sh $ip
+	#bash /local/repository/endpoint-scripts/cwn_half.sh $ip
  	bash /local/repository/endpoint-scripts/cwn.sh $ip
 	break
 	fi
