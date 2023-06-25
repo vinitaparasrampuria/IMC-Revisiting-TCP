2fe2757ca346d99b5268ddb69f83ae0db465772b
Vinita Parasrampuria <91571551+vinitaparasrampuria@users.noreply.github.com>
2023-06-24 23:53:40 -0400

    changed headers of csv file

--- cloudlab-scripts/get_mathis_constant.py
+++ cloudlab-scripts/get_mathis_constant.py
@@ -36,14 +36,14 @@ csv_filename="/local/repository/cloudlab-scripts/packet_loss.csv"
 if not os.path.isfile(csv_filename):
     with open(csv_filename, 'a', newline='') as csvfile:
       writer = csv.writer(csvfile)
-      header = 'port', 'data_seg_out', 'mean_rtt', 'bandwidth_port', 'port_retrans1', 'port_retrans2', 'cwn_half_port', 'packet_loss1', 'x1', 'packet_loss2', 'x2', 'cwnd_half_rate1','x3', 'cwnd_half_rate2', 'x4', 'ratio1', 'ratio2'
+      header = 'port', 'mean_rtt', 'bandwidth_port', 'port_retrans2', 'cwn_half_port', 'packet_loss2', 'x2', 'cwnd_half_rate1','x3', 'ratio2'
       writer.writerow(header)
    
 output_filename='/local/repository/cloudlab-scripts/output_mathis_C.csv'
 if not os.path.isfile(output_filename):
     with open(output_filename, 'a', newline='') as csvfile:
       writer = csv.writer(csvfile)
-      header = 'total_ports', 'ports', 'sum(y_values)', 'total_cwnd_half', 'total_retransmission1', 'total_retransmission2', 'total_retransmission1/total_cwnd_half', 'total_retransmission2/total_cwnd_half', 'np.nanmean(list_ratio1)', 'np.nanmean(list_ratio2)', 'reg_simple1.intercept_', 'reg_simple1.coef_[0]', 'reg_simple2.intercept_', 'reg_simple2.coef_[0]', 'reg_simple3.intercept_', 'reg_simple3.coef_[0]', 'reg_simple4.intercept_', 'reg_simple4.coef_[0]', 'router_dropped', 'router_sent', 'router_dropped/total_cwnd_half'
+      header = 'total_ports', 'ports', 'sum(y_values)', 'total_cwnd_half', 'total_retransmission2', 'total_retransmission2/total_cwnd_half', 'np.nanmean(list_ratio2)', 'reg_simple2.intercept_', 'reg_simple2.coef_[0]', 'reg_simple3.intercept_', 'reg_simple3.coef_[0]', 'router_dropped', 'router_sent', 'router_dropped/total_cwnd_half'
       writer.writerow(header)
 
 
