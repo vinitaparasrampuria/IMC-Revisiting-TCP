9970fbbf4501d7bc6735c651dc0f23a2cc1c770c
Vinita Parasrampuria <91571551+vinitaparasrampuria@users.noreply.github.com>
2023-08-22 19:35:45 -0400

    added data_seg_out and retrans extraction

--- endpoint-scripts/process_cwn_file.py
+++ endpoint-scripts/process_cwn_file.py
@@ -26,28 +26,33 @@ with open("sender-cwn1-10.10.2.1"+ip+"-file.txt", 'r') as file:
       cwnd_match = re.search(r"cwnd:(\d+)", line2)
       if cwnd_match:
           cwnd = int(cwnd_match.group(1))
-          print(f"cwnd value: {cwnd}")
       
       rtt_match = re.search(r"rtt:(\d+)", line2)
       if rtt_match:
           rtt = int(rtt_match.group(1))
-          print(f"rtt value: {rtt}")
       else:
           rtt=None
 
       data_seg_out_match = re.search(r"data_segs_out:(\d+)", line2)
-      if rtt_match:
+      if data_seg_out_match:
           data_segs_out = int(data_seg_out_match.group(1))
       else:
           data_segs_out = None
               
-      retrans_match = re.search(r"retrans:(\d+)", line2)
-      if retrans_match:
-          retrans = int(retrans_match.group(1))
+      #retrans_match = re.search(r"retrans:(\d+)", line2)
+      retrans_index = line2.find("retrans:")  # Find the index of "retrans:"
+      #print(retrans_index)
+      if retrans_index>0:
+        slash_index = line2.find("/", retrans_index)  # Find the index of the next '/' after "retrans:"
+        value_after_slash = line2[slash_index + 1:line2.find(" ", slash_index)]  
+        retrans = value_after_slash
       else:
           retrans = None
 
-        
+      with open(output_filename, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        columns = ip1_match.group(1)+ip2_match.group(1), cwnd , rtt, data_segs_out, retrans
+        writer.writerow(columns)
 
       with open(output_filename, 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
