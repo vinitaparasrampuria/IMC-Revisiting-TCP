2d1e903f5ee883b98e8b3bf977dc48737564df22
Vinita Parasrampuria <91571551+vinitaparasrampuria@users.noreply.github.com>
2023-09-04 04:51:42 -0400

    changed xticks

--- cloudlab-scripts/Intra_cca_JFI_plots.py
+++ cloudlab-scripts/Intra_cca_JFI_plots.py
@@ -9,8 +9,8 @@ ind = np.arange(N)
 width = 0.25
 
 dict_scale={'Edge':[0,50],'Intermediate':[90,500],'Core':[950,5000]}
+dict_flows={'Edge':[10,30,50],'Intermediate':[100,300,500],'Core':[1000,3000,5000]}
 rtt=[20,100,200]
-colors=['royalblue','tomato','mediumspringgreen']
 
 jfi_dat = pd.read_csv("JFI_CloudLab.csv", header=0, names=['cca', 'duration','rtt','BW','sq_BW', 'flow_count', 'jfi'])
 cca=pd.unique(jfi_dat.cca)
@@ -18,10 +18,10 @@ flows=pd.unique(jfi_dat.flow_count)
 
 with PdfPages("JFI_plot.pdf") as pdf:
   for i,c in enumerate(cca):
-    plt.rcParams['figure.figsize'] = (5,3)
-    plt.rcParams['axes.axisbelow'] = True
-    plt.grid(axis='x')
     for key in dict_scale:
+        plt.rcParams['figure.figsize'] = (5,3)
+        plt.rcParams['axes.axisbelow'] = True
+        plt.grid(axis='y')
         dat_cca=jfi_dat[(jfi_dat['cca'] == c)]
         dat_cca.sort_values(by=['flow_count'])
         xvals = dat_cca.jfi[(dict_scale[key][0] <= jfi_dat['flow_count']) & (jfi_dat['flow_count'] <= dict_scale[key][1]) & (jfi_dat['rtt']==20)]
@@ -34,8 +34,8 @@ with PdfPages("JFI_plot.pdf") as pdf:
         plt.xlabel("Flow Count")
         plt.ylabel('JFI')
         plt.title(key+"Scale, "+"CCA-"+c)        
-        plt.xticks(ind+width,['10', '30', '50'])
-        plt.legend( (bar1,bar2, bar3), ('20ms', '100ms', '200ms'), bbox_to_anchor=(1, 0.5) )   
+        plt.xticks(ind+width,dict_flows[key])
+        plt.legend( (bar1,bar2, bar3), ('20ms', '100ms', '200ms'), bbox_to_anchor=(1, 0.5), frameon=False )   
         pdf.savefig(bbox_inches="tight")
         plt.show()
         plt.close()
