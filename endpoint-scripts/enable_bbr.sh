b1c3639898e26fdd2ed7d8c3a105b52c47fb4a8d
Vinita Parasrampuria <91571551+vinitaparasrampuria@users.noreply.github.com>
2024-02-23 00:28:58 -0500

    Create enable_bbr.sh

---
+++ endpoint-scripts/enable_bbr.sh
@@ -0,0 +1,10 @@
+sudo su
+sudo apt update
+apt install -y --install-recommends linux-generic-hwe-16.04
+echo "tcp_bbr" >> /etc/modules-load.d/modules.conf
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+sysctl net.ipv4.tcp_available_congestion_control
+sysctl net.ipv4.tcp_congestion_control
+lsmod | grep bbr
