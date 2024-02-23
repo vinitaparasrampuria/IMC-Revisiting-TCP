c84efcf305186441fec94b94879a59a12bf2ad68
Vinita Parasrampuria <91571551+vinitaparasrampuria@users.noreply.github.com>
2024-02-23 01:00:07 -0500

    give root privilege

--- endpoint-scripts/enable_bbr.sh
+++ endpoint-scripts/enable_bbr.sh
@@ -1,10 +1,9 @@
-sudo su
 sudo apt update
-apt install -y --install-recommends linux-generic-hwe-16.04
-echo "tcp_bbr" >> /etc/modules-load.d/modules.conf
-echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-sysctl -p
-sysctl net.ipv4.tcp_available_congestion_control
-sysctl net.ipv4.tcp_congestion_control
-lsmod | grep bbr
+sudo apt install -y --install-recommends linux-generic-hwe-16.04
+sudo echo "tcp_bbr" >> /etc/modules-load.d/modules.conf
+sudo echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+sudo echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sudo sysctl -p
+sudo sysctl net.ipv4.tcp_available_congestion_control
+sudo sysctl net.ipv4.tcp_congestion_control
+sudo lsmod | grep bbr
