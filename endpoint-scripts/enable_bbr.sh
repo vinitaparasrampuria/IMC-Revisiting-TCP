799cc2515502f944d166a2b8ed3136cbc75f6ee2
Vinita Parasrampuria <91571551+vinitaparasrampuria@users.noreply.github.com>
2024-02-23 01:03:58 -0500

    added sudo to tee command

--- endpoint-scripts/enable_bbr.sh
+++ endpoint-scripts/enable_bbr.sh
@@ -1,8 +1,8 @@
 sudo apt update
 sudo apt install -y --install-recommends linux-generic-hwe-16.04
-sudo echo "tcp_bbr" >> /etc/modules-load.d/modules.conf
-sudo echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-sudo echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+echo "tcp_bbr" | sudo tee -a /etc/modules-load.d/modules.conf
+echo "net.core.default_qdisc=fq" | sudo tee -a /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
 sudo sysctl net.ipv4.tcp_available_congestion_control
 sudo sysctl net.ipv4.tcp_congestion_control
